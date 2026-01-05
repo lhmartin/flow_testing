156c8e4a6df7ea9df533594bf5846c75697ca7d8
Luke Martin <lukechile@gmail.com>
2026-01-05 13:22:49 -0500

    start flow

--- flow_testing/flow/base.py
+++ flow_testing/flow/base.py
@@ -9,3 +9,45 @@ class Sampleable(ABC):
     @abstractmethod
     def sample(self, n_samples: int) -> torch.Tensor:
         raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def dim(self) -> int:
+        raise NotImplementedError
+
+class Alpha(ABC):
+    """
+    Base class for all alphas.
+    """
+    @abstractmethod
+    def __init__(self, alpha : float):
+        self.alpha = alpha
+
+    @abstractmethod
+    def __call__(self, time: float) -> float:
+        raise NotImplementedError
+
+class Beta(ABC):
+    """
+    Base class for all betas.
+    """
+    @abstractmethod
+    def __init__(self, beta : float):
+        self.beta = beta
+
+    @abstractmethod
+    def __call__(self, time: float) -> float:
+        raise NotImplementedError
+
+class Flow(ABC):
+    """
+    Base class for all flows.
+    """
+    @abstractmethod
+    def __init__(self, alpha : float, beta : float):
+        self.alpha = alpha
+        self.beta = beta
+
+    @abstractmethod
+    def sample(self, n_samples: int) -> torch.Tensor:
+        raise NotImplementedError
--- flow_testing/flow/r3_flow.py
+++ flow_testing/flow/r3_flow.py
@@ -1,15 +1,41 @@
-from flow_testing.flow.sampleable import Sampleable
+from flow_testing.flow.base import Sampleable, Flow, Alpha, Beta
+from torch.distributions import MultivariateNormal
 import torch
 
-class R3Flow(torch.nn.Module):
+class GaussianDistribution(Sampleable, torch.nn.Module):
     """
-    Flows
+    Class that represents a Gaussian Distribution.
     """
 
-class R3FlowSampler(Sampleable):
-
-    def __init__(self, flow: R3Flow):
-        self.flow = flow
+    def __init__(self, mean: torch.Tensor, cov: torch.Tensor):
+        """
+        Parameters:
+        -----------
+        mean : torch.Tensor
+            The mean of the Gaussian distribution.
+            Expected shape: [dim]
+        cov : torch.Tensor
+            The covariance matrix of the Gaussian distribution.
+            Expected shape: [dim, dim]
+        """
+        super().__init__()
+        self.register_buffer('mean', mean)
+        self.register_buffer('cov', cov)
 
     def sample(self, n_samples: int) -> torch.Tensor:
-        return self.flow.sample(n_samples)
+        return self.distribution.sample((n_samples,))
+
+    @property
+    def dim(self) -> int:
+        return self.mean.shape[0]
+
+    @property
+    def distribution(self) -> MultivariateNormal:
+        return MultivariateNormal(self.mean, self.cov)
+
+class R3Flow(Sampleable, Flow):
+    def __init__(self, alpha : Alpha, beta : Beta):
+        super().__init__(alpha, beta)
+
+    def sample(self, n_samples: int) -> torch.Tensor:
+        return self.alpha(self.beta.sample(n_samples))
